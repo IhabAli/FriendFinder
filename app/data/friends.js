984dc65118fdb0f33cbb797cefa25b37286555bb
Ihab Ali <Ihab Ali>
2017-05-10 13:25:27 -0400

    complete

---
+++ app/data/friends.js
@@ -0,0 +1,43 @@
+var friends = [ {
+    "name" : "Robert",
+    "photo" : "http://www.wonderslist.com/wp-content/uploads/2016/10/Most-Handsome-Men-Robert-Pattinson.jpg",
+    "scores" : [5,4,2,2,1,1,5,1,3,5]
+}, {
+    "name" : "Kristen",
+    "photo" : "http://www.newyorker.com/wp-content/uploads/2015/04/Brody-Kristen-Stewart-and-the-Movie-Actors-Dilemma-1200.jpg",
+    "scores" : [3,4,2,4,4,1,4,3,2,1]
+}, {
+    "name" : "Taylor",
+    "photo" : "http://vignette3.wikia.nocookie.net/selenagomez/images/9/91/Taylor-lautner-net-worth3.jpg",
+    "scores" : [5,5,5,5,5,5,5,3,3,5]
+}, {
+    "name" : "Russel",
+    "photo" : "https://s-media-cache-ak0.pinimg.com/originals/85/44/14/8544147d00ea26d2d14a22bfeb37b8e2.jpg",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "Gal",
+    "photo" : "http://www.protothema.gr/files/1/2016/03/27/wonder_woman.jpg",
+    "scores" : [1,2,3,3,4,5,2,3,4,5]
+}, {
+    "name" : "Chris",
+    "photo" : "https://i.annihil.us/u/prod/marvel/movies/civilwar/images/captainamerica_hero.png",
+    "scores" : [4,1,5,3,4,1,1,2,4,5]
+}, {
+    "name" : "Jackman",
+    "photo" : "https://static.comicvine.com/uploads/original/2/26163/552858-hhhhhhhhhhhhhhhhh.jpg",
+    "scores" : [3,4,2,4,4,3,3,2,3,4]
+}, {
+    "name" : "Bruce",
+    "photo" : "http://images6.fanpop.com/image/photos/36000000/Bruce-Wayne-image-bruce-wayne-36050164-967-1450.jpg",
+    "scores" : [5,4,2,5,5,5,2,1,3,5]
+}, {
+    "name" : "Lee",
+    "photo" : "http://cdn.scrapdigest.com/wp-content/uploads/2015/12/bruce-lee1.jpg",
+    "scores" : [1,5,1,3,5,4,3,2,1,1]
+}, {
+    "name" : "D.P.",
+    "photo" : "http://s3.foxmovies.com/foxmovies/production/films/103/images/featured_content/111-front.jpg",
+    "scores" : [1,3,3,1,2,4,4,1,1,1]
+},
+    
+    module.exports = friends;
